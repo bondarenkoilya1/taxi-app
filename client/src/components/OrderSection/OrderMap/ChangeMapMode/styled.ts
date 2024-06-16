9fe518c3b2bade9deaae7e678b092c793a393897
Ilya Bondarenko <bondarenkoilya2@gmail.com>
2024-06-16 13:42:55 +0300

    put the map mode changer into a separate component

---
+++ client/src/components/OrderSection/OrderMap/ChangeMapMode/styled.ts
@@ -0,0 +1,20 @@
+import styled from "@emotion/styled";
+
+export const ChangeMapModeContainerStyled = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 auto 30px auto;
+`;
+
+export const ChangeMapModeButtonStyled = styled.button`
+  width: 300px;
+  font-size: 20px;
+  text-transform: uppercase;
+  padding: 8px 0;
+`;
+
+export const ChangeMapModeCheckboxStyled = styled.input`
+  margin-left: 10px;
+  width: 50px;
+  height: 40px;
+`;
--- client/src/components/OrderSection/OrderMap/OrderMap.tsx
+++ client/src/components/OrderSection/OrderMap/OrderMap.tsx
@@ -5,6 +5,9 @@ import { Marker } from "components/ui/Marker";
 import { MAP_MODES } from "constants/index";
 import { type LocationObject, MapProps } from "types";
 
+import { ChangeMapMode } from "./ChangeMapMode";
+import { MapContainerStyled } from "./styled";
+
 export const OrderMap: React.FC<MapProps> = ({ markers, onMarkerAdd, mode, toggleMode }) => {
   const center = { lat: 53.35, lng: -6.26 };
 
@@ -19,38 +22,9 @@ export const OrderMap: React.FC<MapProps> = ({ markers, onMarkerAdd, mode, toggl
     [mode, onMarkerAdd]
   );
 
-  // TODO: Get rid of these divs
   return (
-    <div
-      style={{
-        width: "100%",
-        display: "flex",
-        flex: 1,
-        flexDirection: "column"
-      }}>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          margin: "0 auto 30px auto"
-        }}>
-        <button
-          type="button"
-          onClick={toggleMode}
-          style={{
-            width: "300px",
-            fontSize: "20px",
-            textTransform: "uppercase",
-            padding: "8px 0"
-          }}>
-          I want to set markers
-        </button>
-        <input
-          type="checkbox"
-          style={{ marginLeft: "10px", width: "50px", height: "40px" }}
-          checked={mode === MAP_MODES.SET_MARKER}
-        />
-      </div>
+    <MapContainerStyled>
+      <ChangeMapMode mode={mode} toggleMode={toggleMode} />
       <GoogleMap
         zoom={11}
         center={center}
@@ -58,10 +32,9 @@ export const OrderMap: React.FC<MapProps> = ({ markers, onMarkerAdd, mode, toggl
         options={{ disableDefaultUI: true, keyboardShortcuts: false }}
         onClick={OnMapClick}>
         {markers.map((position) => (
-          // TODO: deal with the key attribute
-          <Marker position={position} key={position.lat} />
+          <Marker position={position} key={crypto.randomUUID()} />
         ))}
       </GoogleMap>
-    </div>
+    </MapContainerStyled>
   );
 };

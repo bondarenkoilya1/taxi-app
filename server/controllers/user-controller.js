282ff01fbdf4057116df85c05d3240c1c58493da
Ilya <bondarenkoilya2@gmail.com>
2024-07-10 12:11:39 +0300

    create user controller

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,38 @@
+class UserController {
+  async registration(req, res, next) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async login(req, res, next) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async logout(req, res, next) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async activate(req, res, next) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async refresh(req, res, next) {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+module.exports = new UserController();

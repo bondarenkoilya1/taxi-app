2ec7c9bde748ef924287d00a650798025be44fbc
Ilya <bondarenkoilya2@gmail.com>
2024-08-18 18:23:32 +0300

    add types for future form

--- client/src/types/components/ui/auth.ts
+++ client/src/types/components/ui/auth.ts
@@ -1,3 +1,11 @@
+export type FormTypes = {
+  formType: "login" | "register";
+};
+
+export type FormStyles = {
+  formStyle: "primary" | "secondary";
+};
+
 export type FormTextProps = {
   centered?: boolean;
   marginRight?: boolean;
